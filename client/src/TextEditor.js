4cf455727a43a3f31599cebc470ec3e6545368b4
Ankit <imankitpandey31@gmail.com>
2021-07-09 00:24:10 +0530

    added css

--- client/src/TextEditor.js
+++ client/src/TextEditor.js
@@ -16,7 +16,7 @@ export default function TextEditor() {
     },[])
 
     return (
-        <div id="container" ref={wrapperRef}>
+        <div className=" container" ref={wrapperRef}>
                 TextEdditor
         </div>
     )
