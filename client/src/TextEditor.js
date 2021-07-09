5772d4a3ddf9a89f8755d3ab1a16847386cd7dea
Ankit <imankitpandey31@gmail.com>
2021-07-09 15:07:26 +0530

    connection with socketio

--- client/src/TextEditor.js
+++ client/src/TextEditor.js
@@ -1,6 +1,7 @@
-import {useRef,useCallback} from "react"
+import {useCallback,useEffect,useState} from "react"
 import Quill from "quill"
 import "quill/dist/quill.snow.css"
+import { io } from 'socket.io-client'
 
 const TOOLBAR_OPTIONS = [
     [{header: [1,2,3,4,5,6,false]}], 
@@ -16,16 +17,38 @@ const TOOLBAR_OPTIONS = [
 ]
 
 export default function TextEditor() {
+    const [socket,setSocket] = useState()
+    const [quill,setQuill] = useState()
+    useEffect(()=>{
+       const s = io("http://localhost:3001")
+       setSocket(s)
+       return ()=>{
+           s.disconnect() 
+       }
+    },[])
+
+    useEffect(()=>{
+        if(socket == null || quill == null ) return
+        const handler = (delta,oldDelta ,source)=>{
+            if(source !== 'user') return   
+            socket.emit("send-changes",delta)
+        }
+        quill.on('text-change',handler)
+
+        return ()=>{
+            quill.off('text-change',handler)
+        }
+    },[socket,quill])
 
     const wrapperRef = useCallback((wrapper) => {
         if(wrapper === null) {
             return
         }
-        wrapper.innerHTML = ""
+         wrapper.innerHTML = ""
         const editor = document.createElement("div")
         wrapper.append(editor)
-        new Quill( editor ,{ theme: "snow" , modules: {toolbar: TOOLBAR_OPTIONS}})
-        
+        const q = new Quill( editor ,{ theme: "snow" , modules: {toolbar: TOOLBAR_OPTIONS}})
+        setQuill(q)
     },[])
 
     return (
@@ -34,3 +57,4 @@ export default function TextEditor() {
         </div>
     )
 }
+
